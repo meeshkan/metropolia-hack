a3032578db82058b2ffd0963aed401c1c368417b
Mike Solomon <mike@mikesolomon.org>
2018-10-02 18:06:43 +0300

    Commits loss

--- js/src/App.js
+++ js/src/App.js
@@ -9,37 +9,45 @@ class App extends Component {
     super();
 
     const model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-    // Prepare the model for training: Specify the loss and the optimizer.
-    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
+    
     // Generate some synthetic data for training.
     const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
     const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
     this.state = {
-      foo:1,
+      loss: 100000,
       xs,
       ys,
       model
     }
   }
 
+  async componentDidMount() {
+    this.state.model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+    // Prepare the model for training: Specify the loss and the optimizer.
+    this.state.model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+    for (let i = 1; i < 100 ; ++i) {
+      const history = await this.state.model.fit(this.state.xs, this.state.ys, {
+        epochs: 1,
+      });
+      this.setState({loss: history.history.loss[0]})
+   }
+    const output = this.state.model.predict(tf.tensor2d([1], [1,1]));
+    this.setState({
+      myStateVar: output.dataSync()[0]
+    });
+  }
+
   render() {
     // Train the model using the data.
-    this.state.model.fit(this.state.xs, this.state.ys, {epochs: 10}).then(() => {
-      // Use the model to do inference on a data point the model hasn't seen before:
-      // Open the browser devtools to see the output
-      //console.log(model.predict(tf.tensor2d([5], [1, 1])));
-      this.setState({foo: ''+this.state.model.predict(tf.tensor2d([5], [1, 1])) });
-    });
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Meeshkan's AI Class</h1>
         </header>
-        <p>{this.state.foo}</p>
+        <p>{this.state.loss}</p>
       </div>
     );
   }
