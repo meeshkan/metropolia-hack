8d64c2b26acdbfe317d8e059ee88e66ab2db7dc8
Mike Solomon <mike@mikesolomon.org>
2018-10-02 17:34:17 +0300

    fun times with python

--- js/src/App.js
+++ js/src/App.js
@@ -7,12 +7,7 @@ class App extends Component {
 
   constructor() {
     super();
-    this.state = {
-      foo:1
-    }
-  }
 
-  render() {
     const model = tf.sequential();
     model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
@@ -22,13 +17,21 @@ class App extends Component {
     // Generate some synthetic data for training.
     const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
     const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+    this.state = {
+      foo:1,
+      xs,
+      ys,
+      model
+    }
+  }
 
+  render() {
     // Train the model using the data.
-    model.fit(xs, ys, {epochs: 10}).then(() => {
+    this.state.model.fit(this.state.xs, this.state.ys, {epochs: 10}).then(() => {
       // Use the model to do inference on a data point the model hasn't seen before:
       // Open the browser devtools to see the output
       //console.log(model.predict(tf.tensor2d([5], [1, 1])));
-      this.setState({foo: ''+model.predict(tf.tensor2d([5], [1, 1])) });
+      this.setState({foo: ''+this.state.model.predict(tf.tensor2d([5], [1, 1])) });
     });
     return (
       <div className="App">
