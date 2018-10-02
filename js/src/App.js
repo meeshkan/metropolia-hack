05e041c953fb2e43d7e057ae6ef58c24cc65c933
Mike Solomon <mike@mikesolomon.org>
2018-10-02 18:27:49 +0300

    giant monstrosity

--- js/src/App.js
+++ js/src/App.js
@@ -22,7 +22,42 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    this.state.model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+    /*
+    self.D = Sequential()
+depth = 64
+dropout = 0.4
+# In: 28 x 28 x 1, depth = 1
+# Out: 14 x 14 x 1, depth=64
+input_shape = (self.img_rows, self.img_cols, self.channel)
+self.D.add(Conv2D(depth*1, 5, strides=2, input_shape=input_shape,\
+padding='same', activation=LeakyReLU(alpha=0.2)))
+self.D.add(Dropout(dropout))
+self.D.add(Conv2D(depth*2, 5, strides=2, padding='same',\
+activation=LeakyReLU(alpha=0.2)))
+self.D.add(Dropout(dropout))
+self.D.add(Conv2D(depth*4, 5, strides=2, padding='same',\
+activation=LeakyReLU(alpha=0.2)))
+self.D.add(Dropout(dropout))
+self.D.add(Conv2D(depth*8, 5, strides=1, padding='same',\
+activation=LeakyReLU(alpha=0.2)))
+self.D.add(Dropout(dropout))
+# Out: 1-dim probability
+self.D.add(Flatten())
+self.D.add(Dense(1))
+self.D.add(Activation('sigmoid'))
+self.D.summary()
+    */
+   this.state.model.add(tf.layers.conv2d({
+    filters: 64,
+    kernelSize: [5,5],
+    strides: 2,
+    inputShape: [10, 128, 128, 3]
+   })); // 2dconv
+   this.state.model.add(tf.layers.leakyReLU({
+     alpha: 0.2
+   })); // leaky relu
+
+    /*this.state.model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
     // Prepare the model for training: Specify the loss and the optimizer.
     this.state.model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
@@ -36,7 +71,7 @@ class App extends Component {
     const output = this.state.model.predict(tf.tensor2d([1], [1,1]));
     this.setState({
       myStateVar: output.dataSync()[0]
-    });
+    });*/
   }
 
   render() {
